2718d911dd56ead9031a7c0b008c17aa13a2b996
alagache <alagache@student.42.fr>
2023-07-23 22:39:53 +0200

    create pair of courses ready to be plotted

--- scatter.py
+++ scatter.py
@@ -2,18 +2,27 @@
 
 import sys
 import pandas
+import getopt
 
 from handle_data import create_dataframe
 import matplotlib.pyplot as pyplot
 
-print(sys.argv)
+options, args = getopt.getopt(sys.argv[1:], '', ['show'])
+nbr_arg = len(args)
+nbr_opt = len(options)
 
-nbr_arg = len(sys.argv)
+show_plot = False
+if nbr_opt > 0:
+    for option in options:
+        if option[0] == '--show':
+            show_plot = True
 
 dataset = pandas.DataFrame()
-if nbr_arg >= 2:
-    print(sys.argv[1])
-    dataset = create_dataframe(sys.argv[1])
+if nbr_arg >= 1:
+    dataset = create_dataframe(args[0])
+else:
+    print("no dataset provided\nusage...")
+    exit(1)
 
 numerical_features = dataset.select_dtypes(include=["float64"])
 course_with_houses = pandas.concat([numerical_features, dataset["Hogwarts House"]], axis=1)
@@ -23,21 +32,45 @@ slytherin = course_with_houses.loc[course_with_houses["Hogwarts House"] == "Slyt
 hufflepuff = course_with_houses.loc[course_with_houses["Hogwarts House"] == "Hufflepuff"]
 gryffindor = course_with_houses.loc[course_with_houses["Hogwarts House"] == "Gryffindor"]
 
+# Get a list of course pairs (Astro, Herbo)
+print("START\n")
+
+courses_list = list(numerical_features.items())
+courses_nbr = len(courses_list)
+# for t in courses_list:
+#     print(t[0])
+courses_pairs = []
+
+for i in range(0, courses_nbr, 1):
+    # Get list of all course except of index i
+    other_courses = [courses_list[j] for j in range(0, courses_nbr, 1) if i != j]
+    # tuple of course[i], all other courses
+    app = (courses_list[i], other_courses)
+    courses_pairs.append(app)
+
+for given_course, given_courses in courses_pairs:
+    for course in given_courses:
+        print(f"{given_course[0]} - {course[0]}")
+    print("\n")
+
+# TODO scatter plot the pairs of classes
+exit(0)
 for label, _ in numerical_features.items():
 
     pyplot.style.use('gruvbox.mplstyle')
     pyplot.title(str(label))
 
-    pyplot.hist(ravenclaw[label], alpha=0.5, label="Ravenclaw")
-    pyplot.hist(slytherin[label], alpha=0.5, label="Slytherin")
-    pyplot.hist(hufflepuff[label], alpha=0.5, label="Hufflepuff")
-    pyplot.hist(gryffindor[label], alpha=0.5, label="Gryffindor")
+    # pyplot.hist(ravenclaw[label], alpha=0.5, label="Ravenclaw")
+    # pyplot.hist(slytherin[label], alpha=0.5, label="Slytherin")
+    # pyplot.hist(hufflepuff[label], alpha=0.5, label="Hufflepuff")
+    # pyplot.hist(gryffindor[label], alpha=0.5, label="Gryffindor")
 
-    pyplot.legend(loc='best')
+    # pyplot.legend(loc='best')
 
-    # filename = f'ressources/{label}.png'
-    filename = f'static/Image/hist/{label}.png'
-    pyplot.savefig(filename, format="png")
+    filename = f'static/Image/scatter/{label}.png'
+    # pyplot.savefig(filename, format="png")
+    # print(f'created {filename}')
 
-    pyplot.show()
-    # break
+    if show_plot == True:
+        pyplot.show()
+    pyplot.close()
