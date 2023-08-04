9abee28883d94e7f90c5b39c56635a99ced5b237
alagache <alagache@student.42.fr>
2023-08-04 07:32:00 +0200

    sort dataframe by column name

--- handle_data.py
+++ handle_data.py
@@ -7,5 +7,6 @@ def create_dataframe(csv_string):
     print(csv_string)
     csv_dataset = pandas.read_csv(csv_string)
     dataset = pandas.DataFrame(csv_dataset)
+    dataset.sort_index(axis=1, inplace=True)
 
     return dataset
