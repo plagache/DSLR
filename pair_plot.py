85ac2928e1b0d6417058543d3ad7ed05450c25dc
alagache <alagache@student.42.fr>
2023-09-14 19:02:19 +0000

    correct house name
    use subject list to feed pair plot

--- pair_plot.py
+++ pair_plot.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import pandas
 import seaborn
 import argparse
 import matplotlib.pyplot as pyplot
@@ -9,7 +8,7 @@ from handle_data import create_dataframe, blue, green, yellow, red
 color_palette = {
         "Gryffindor": red,
         "Slytherin": green,
-        "Hufflepuf": yellow,
+        "Hufflepuff": yellow,
         "Ravenclaw": blue
         }
 
@@ -22,20 +21,27 @@ args = parser.parse_args()
 
 dataset = create_dataframe(args.filename)
 
-dataset.drop(columns=['First Name','Last Name','Birthday','Best Hand'])
+subjects = [
+        'Arithmancy',
+        'Astronomy',
+        'Herbology',
+        'Defense Against the Dark Arts',
+        'Divination',
+        'Muggle Studies',
+        'Ancient Runes',
+        'History of Magic',
+        'Transfiguration',
+        'Potions',
+        'Care of Magical Creatures',
+        'Charms',
+        'Flying'
+        ]
+splot = seaborn.pairplot(dataset, vars=subjects, hue="Hogwarts House", palette=color_palette, diag_kind="hist")
 
-print("PLOT")
-splot = seaborn.pairplot(dataset, hue="Hogwarts House", palette=color_palette, diag_kind="hist")
-
-print("SAVEFIG")
 filename = 'static/Image/pair/pairplot.png'
 splot.savefig(filename)
-
 print(f'created {filename}')
-# pyplot.show()
-print("CLOSE")
-pyplot.close()
 
 if args.show == True:
-    print("HERE SHOW TRUE")
-    exit(0)
+    pyplot.show()
+pyplot.close()
