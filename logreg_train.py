9c7a273fa6aeefe6915c846452554cf74f59fd36
plgch <plagache@protonmail.com>
2024-01-09 01:17:11 +0100

    add sgd and gd to optim;

--- logreg_train.py
+++ logreg_train.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas
 from nn import Neuron
-from numpy import random
+from optim import sgd, gd
 from handle_data import create_dataframe, classer
 import argparse
 
@@ -13,33 +13,6 @@ args = parser.parse_args()
 dataset = create_dataframe(args.filename)
 
 
-def optimizer(tensor, neuron, ys):
-    # Separate tensor and ys here
-    # input: tensor, ys, neuron.weight
-    # output: outputs, diffs, losses
-    row_count = len(tensor)
-    loss = 0
-
-    outputs = neuron.outputs(tensor.T)
-    diffs = outputs - ys
-
-    # for element, y in zip(outputs, ys):
-    #     loss += y * np.log(element) + (1 - y) * np.log(1 - element)
-    # loss = - loss / row_count
-    loss = ys * np.log(outputs) + (1 - ys) * np.log(1 - outputs)
-    loss = - loss / row_count
-
-    # Update weight
-    # inputs: neuron.weight, diffs, tensor
-    # outputs: neuron.weight.updater
-    # diffs @ tensor = [13, 1]
-    diffnp = np.array([diffs]).reshape((1,1))
-    tensornp = np.reshape(tensor, (1,13))
-    derivative = diffnp @ tensornp
-    derivative /= row_count
-    derivative = np.reshape(derivative, (13,))
-    neuron.weight -= learning_rate * derivative
-    return loss
 
 
 houses = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"]
@@ -68,9 +41,10 @@ for index, house in enumerate(houses):
 
     for step in range(total):
         # here we can shuffle a random part of our tensor to make SGD
-        loss = optimizer(tensor[step], neuron, ys[step])
+        loss = sgd(tensor[step], neuron, ys[step], learning_rate)
+        # loss = gd(tensor[step], neuron, ys[step], learning_rate)
         losses.append(loss)
-        if step % 100 == 0 :
+        if step % 100 == 0:
             print(f"step : {step}\nloss : {losses[-1]}\nweight : {neuron.weight}\n")
 
     weights_matrix.append(neuron.weight)
--- optim.py
+++ optim.py
@@ -1,3 +1,4 @@
+import numpy as np
 # differente class of hyperparamater optimization
 
 # https://en.wikipedia.org/wiki/Hyperparameter_optimization
@@ -13,9 +14,54 @@ def Optimizer():
         self.lr = learning_rate
 
 
-# def GradientDescent():
-#
-#
-# def SGD():
-#
-#
+def sgd(tensor, neuron, ys, learning_rate):
+    # Separate tensor and ys here
+    # input: tensor, ys, neuron.weight
+    # output: outputs, diffs, losses
+    row_count = len(tensor)
+    loss = 0
+
+    outputs = neuron.outputs(tensor.T)
+    diffs = outputs - ys
+
+    # for element, y in zip(outputs, ys):
+    #     loss += y * np.log(element) + (1 - y) * np.log(1 - element)
+    # loss = - loss / row_count
+    loss = ys * np.log(outputs) + (1 - ys) * np.log(1 - outputs)
+    loss = - loss / row_count
+
+    # Update weight
+    # inputs: neuron.weight, diffs, tensor
+    # outputs: neuron.weight.updater
+    # diffs @ tensor = [13, 1]
+    diffnp = np.array([diffs]).reshape((1, 1))
+    tensornp = np.reshape(tensor, (1, 13))
+    derivative = diffnp @ tensornp
+    derivative /= row_count
+    derivative = np.reshape(derivative, (13,))
+    neuron.weight -= learning_rate * derivative
+    return loss
+
+
+def gd(tensor, neuron, ys, learning_rate):
+    # Separate tensor and ys here
+    # input: tensor, ys, neuron.weight
+    # output: outputs, diffs, losses
+    row_count = len(tensor)
+    loss = 0
+
+    outputs = neuron.outputs(tensor.T)
+    diffs = outputs - ys
+
+    for element, y in zip(outputs, ys):
+        loss += y * np.log(element) + (1 - y) * np.log(1 - element)
+    loss = - loss / row_count
+
+    # Update weight
+    # inputs: neuron.weight, diffs, tensor
+    # outputs: neuron.weight.updater
+    # diffs @ tensor = [13, 1]
+    derivative = diffs @ tensor
+    derivative /= row_count
+    neuron.weight -= learning_rate * derivative
+    return loss
