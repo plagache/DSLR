c409f442432d4d320ecfe0e4bfba4c2ba970cd3e
plgch <plagache@protonmail.com>
2024-02-12 16:35:36 +0100

    add main start for function call;

--- accuracy_test.py
+++ accuracy_test.py
@@ -3,17 +3,20 @@ from data_preprocessing import create_dataframe
 from sklearn.metrics import accuracy_score
 from variables import labels_column
 
-parser = argparse.ArgumentParser(description="A simple python program to test the accuracy of a given csv dataset")
-parser.add_argument('samples', help='the test samples csv file')
-parser.add_argument('prediction', help='the houses.csv file')
-args = parser.parse_args()
+def test_accuracy(samples, prediction, labels_column):
+    y_true = samples[labels_column].tolist()
+    y_prediction = prediction[labels_column].tolist()
 
-samples = create_dataframe(args.samples)
-prediction = create_dataframe(args.prediction)
+    accuracy = accuracy_score(y_true, y_prediction)
+    return accuracy
 
-y_true = samples[labels_column].tolist()
-y_prediction = prediction[labels_column].tolist()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A simple python program to test the accuracy of a given csv dataset")
+    parser.add_argument('samples', help='the test samples csv file')
+    parser.add_argument('prediction', help='the houses.csv file')
+    args = parser.parse_args()
 
-accuracy = accuracy_score(y_true, y_prediction)
-
-print(f"\naccuracy: {accuracy * 100}%")
+    samples = create_dataframe(args.samples)
+    prediction = create_dataframe(args.prediction)
+    accuracy = test_accuracy(samples, prediction, labels_column)
+    print(f"\naccuracy: {accuracy * 100}%")
--- sampler.py
+++ sampler.py
@@ -2,13 +2,6 @@ import argparse
 from data_preprocessing import create_dataframe
 from variables import labels_column
 
-parser = argparse.ArgumentParser(description="A simple python program to sample dataset")
-parser.add_argument('dataset', help='the dataset csv file')
-parser.add_argument('-s', '--size', default=1 , type=float, required=False, help='size of the sample to create')
-args = parser.parse_args()
-
-dataset = create_dataframe(args.dataset)
-
 
 def split_dataframe(dataframe, test_percent: float):
     test_sample = dataframe.groupby(labels_column).sample(frac=test_percent)
@@ -16,10 +9,18 @@ def split_dataframe(dataframe, test_percent: float):
     return test_sample, train_sample
 
 
-if args.size < 1:
-    test_sample, train_sample = split_dataframe(dataset, args.size)
-else:
-    test_sample, train_sample = dataset, dataset
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A simple python program to sample dataset")
+    parser.add_argument('dataset', help='the dataset csv file')
+    parser.add_argument('-s', '--size', default=1 , type=float, required=False, help='size of the sample to create')
+    args = parser.parse_args()
 
-test_sample.to_csv("datasets/dataset_test.csv")
-train_sample.to_csv("datasets/dataset_train.csv")
+    dataset = create_dataframe(args.dataset)
+
+    if args.size < 1:
+        test_sample, train_sample = split_dataframe(dataset, args.size)
+    else:
+        test_sample, train_sample = dataset, dataset
+
+    test_sample.to_csv("datasets/dataset_test.csv")
+    train_sample.to_csv("datasets/dataset_train.csv")
