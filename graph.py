fcb4ecd88429a1cc52d09e139f5d5d15f94e8167
plgch <plagache@protonmail.com>
2024-02-19 19:04:03 +0100

    update graph;
    all class in one graph;

--- graph.py
+++ graph.py
@@ -6,25 +6,30 @@ import numpy as np
 from data_preprocessing import create_dataframe
 from variables import colors
 
-parser = argparse.ArgumentParser(description="A simple python program to print a summary of a given csv dataset")
-parser.add_argument("filename", help="the loss csv file")
-args = parser.parse_args()
 
-dataset = create_dataframe(args.filename)
-
-plt.style.use("gruvbox.mplstyle")
-
-
-def draw_losses(losses, class_name):
+def draw_losses(losses, classes):
     total = len(losses)
 
-    plt.plot(np.linspace(0, total, total), losses, ".", c=colors[class_name])
-    plt.title(class_name)
-    plt.xlabel("step")
-    plt.ylabel("loss")
-    plt.savefig(f"static/Image/loss/{class_name}.png")
+    for class_name, content in losses.items():
+        plt.plot(np.linspace(0, total, total), content, ".", c=colors[class_name])
+        plt.xlabel("step")
+        plt.ylabel("loss")
+    plt.legend(classes)
+    plt.savefig("static/Image/loss/losses.png")
+    plt.show()
     plt.close()
 
 
-for class_name, content in dataset.items():
-    draw_losses(content, class_name)
+if __name__ == "__main__":
+    print("\n------------ Graph -----------")
+
+    parser = argparse.ArgumentParser(description="A simple python program to print a summary of a given csv dataset")
+    parser.add_argument("filename", help="the loss csv file")
+    args = parser.parse_args()
+
+    dataset = create_dataframe(args.filename)
+
+    plt.style.use("gruvbox.mplstyle")
+
+    classes = dataset.columns.tolist()
+    plt = draw_losses(dataset, classes)
